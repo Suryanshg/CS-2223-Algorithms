47863d7bb499f6e583a313b1cb1b697e13da4fec
AlexG1031 <42430291+AlexG1031@users.noreply.github.com>
2019-11-07 23:06:04 -0500

    cleaned up palindromecheck

--- palindromecheck/src/palindromecheck.cpp
+++ palindromecheck/src/palindromecheck.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : palindromecheck.cpp
-// Author      : Suryansh Goyal
+// Author      : Suryansh Goyal and Alex Galvan
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
@@ -8,68 +8,49 @@
 
 #include <iostream>
 #include <string>
-#include <ctype.h>
-#include <stdio.h>
 using namespace std;
 
-string processString (string input);
-string lower(string input);
+string processString(string input);
 
 int main() {
 
 	string input;
-	cout<<"Input String:";
-	getline(cin,input);
-	bool isPal=true;
-	string processedInput=processString(input);
-	int len=processedInput.length();
-	int mid=len/2;
-	for(int i=0;i<mid;i++){
-		if(processedInput[i]!=processedInput[len-1-i]){
-			isPal=false;
+	cout << "Input String:";
+	getline(cin, input); //  removes spaces from string and stores it in input
+	bool isPal = true;
+	string processedInput = processString(input);
+	int len = processedInput.length();
+	cout << processedInput << endl;
+	int mid = len / 2;
+	for (int i = 0; i < mid; i++) {
+		if (processedInput[i] != processedInput[len - 1 - i]) {
+			isPal = false;
 			break;
 		}
 	}
 
-	if(isPal){
-		cout<<"Yay! It's a Palindrome.\n";
-	}
-	else{
-		cout<<"No! It's not a Palindrome.\n";
+	if (isPal) {
+		cout << "Yay! It's a Palindrome.\n";
+	} else {
+		cout << "No! It's not a Palindrome.\n";
 	}
 
 	return 0;
 }
 
-
 /*
- * Processes a string by removing whitespace and extra special characters
+ * Processes a string by removing special characters and lowercasing the uppercase letters
  */
-string processString(string input){
+string processString(string input) {
 	string result;
-	int len=input.length();
-	for(int i=0;i<len;i++){
-		if(isalpha(input[i])!=0){
-			result.push_back(input[i]);
-		}
-	}
-
-	result=lower(result);
-	return result;
-}
-
-/*
- *converts a string to lowercase
- */
-string lower(string input){
-	string result;
-	int len=input.length();
-	for(int i=0;i<len;i++){
-		if(input[i]>='A' && input[i]<='Z'){
-			result.push_back(input[i]+32);
-		}
-		else{
-			result.push_back((input[i]));
+	int len = input.length();
+	for (int i = 0; i < len; i++) {
+		if (isalpha(input[i]) != 0) {
+			if (input[i] >= 'A' && input[i] <= 'Z') {
+				result.push_back(input[i] + 32);
+			} else {
+				result.push_back((input[i]));
+			}
 		}
 	}
 	return result;
