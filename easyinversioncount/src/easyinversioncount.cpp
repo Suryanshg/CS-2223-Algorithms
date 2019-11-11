5447ed8e6e4abde8775929ae80e0b4b21ae2ee91
Suryanshg <Suryanshg47@gmail.com>
2019-11-11 17:35:57 -0500

    Fixed easyinversioncount

--- easyinversioncount/src/easyinversioncount.cpp
+++ easyinversioncount/src/easyinversioncount.cpp
@@ -11,19 +11,43 @@
 using namespace std;
 
 int main() {
+	int inputSize;
+	cout<<"Enter the size of the array you want to build:";
+	cin>>inputSize;
+	int arr[inputSize];
+	int input;
+	bool validInput=false;
+	cout<<"\nLet's make the array. Please don't enter a negative value.\n";
+	for(int i=0;i<inputSize;i++){
+		while(!validInput){
+			cout<<"Enter an integer:";
+			cin>>input;
+
+			if(input>=0){
+				validInput=true;
+			}
+			else{
+				cout<<"Invalid Input! Please try again.";
+			}
+			cout<<"\n";
+		}
+		arr[i]=input;
+		validInput=false;
+
+	}
+	cout<<"Your array is:\n";
+	for(int i=0;i<inputSize;i++){
+		cout<<arr[i]<<" ";
+	}
 	int count = 0;
-	int foo[] = {5,4,3,2,1};
-	int len = sizeof(foo)/sizeof(foo[0]);
-	//int len =*(&foo + 1)-foo;
-	for (int i = 0; i < len; i++) {
-		for (int j = i + 1; j < len; j++) {
-			if (foo[i] > foo[j]) //num out of order
-					{
+
+
+	for (int i = 0; i < inputSize; i++) {
+		for (int j = i + 1; j < inputSize; j++) {
+			if (arr[i] > arr[j]){
 				count++;
-				cout<<"("<<foo[i]<<", "<<foo[j]<<") count: "<<count<<endl;
 			}
 		}
 	}
-	cout << "count: "<<count<<endl;
-	return count;
+	cout << "Number of Inversions: "<<count<<endl;
 }
