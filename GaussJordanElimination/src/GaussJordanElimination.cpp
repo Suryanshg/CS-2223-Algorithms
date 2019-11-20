c2702f941b98848c3b0f70418e16b8e823437e23
Suryanshg <Suryanshg47@gmail.com>
2019-11-19 19:09:59 -0500

    Gauss-Jordan Elimination complete

--- GaussJordanElimination/src/GaussJordanElimination.cpp
+++ GaussJordanElimination/src/GaussJordanElimination.cpp
@@ -11,19 +11,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void betterForwardElimination(double A[][9], double b[], int n);
 void gaussJordanElimination(double A[][9], double b[], int n);
 void printMatrix(double A[][9], int n);
 
 int main() {
 	double A[8][9] = {{ 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 },
-					  { 1.0, 2.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0 },
-					  { 1.0, 1.0, 3.0, 1.0, 1.0, 3.0, 1.0, 1.0 },
-					  { 1.0, 1.0, 1.0, 4.0, 4.0, 1.0, 1.0, 1.0 },
-					  {11.0, 1.0, 1.0, 1.0,	1.0, 1.0, 1.0, 1.0 },
-					  { 1.0, 1.0, 1.0, 1.0,-1.0,-1.0,-1.0,-1.0 },
-					  { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0 },
-					  { 1.0,-1.0, 1.0,-1.0, 1.0,-1.0, 1.0,-1.0 },};
+			{ 1.0, 2.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0 },
+			{ 1.0, 1.0, 3.0, 1.0, 1.0, 3.0, 1.0, 1.0 },
+			{ 1.0, 1.0, 1.0, 4.0, 4.0, 1.0, 1.0, 1.0 },
+			{11.0, 1.0, 1.0, 1.0,	1.0, 1.0, 1.0, 1.0 },
+			{ 1.0, 1.0, 1.0, 1.0,-1.0,-1.0,-1.0,-1.0 },
+			{ 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0 },
+			{ 1.0,-1.0, 1.0,-1.0, 1.0,-1.0, 1.0,-1.0 },};
 
 	double b[8] = { 0.0, 0.0, 0.0, 0.0, 20.0, 34.0, -51.0, -6.0 };
 	int n = sizeof(A) / sizeof(A[0]);
@@ -32,39 +31,12 @@ int main() {
 	//betterForwardElimination(A, b, n);
 	gaussJordanElimination(A, b, n);
 	//printMatrix(A, n);
-}
 
-/*
- * n is the order of the matrix A
- */
-void betterForwardElimination(double A[][9], double b[], int n) {
-	int pivotRow;
-	double temp;
-	for (int i = 0; i < n; i++) { //Augments the matrix b to A
-		A[i][n] = b[i];
-	}
-
-	for (int i = 0; i < n - 1; i++) {
-		pivotRow = i;
-		for (int j = i + 1; j < n; j++) {
-			if (abs(A[j][i]) > abs(A[pivotRow][i])) {
-				pivotRow = j;
-			}
-		}
-		for (int k = i; k < n + 1; k++) {
-			swap(A[i][k], A[pivotRow][k]);
-		}
-		for (int j = i + 1; j < n; j++) {
-			temp = A[j][i] / A[i][i];
-			for (int k = i; k < n + 1; k++) {
-				A[j][k] = round((A[j][k] - A[i][k] * temp) * 10000) / 10000;
-			}
-		}
-	}
-	cout << "Printing out BetterForward Elimination" << endl;
+	cout << "Result of Guass-Jordan Elimination Algorithm on the provided matrix is" << endl;
 	printMatrix(A, n);
 	cout << endl;
 }
+
 
 /*
  *
@@ -91,16 +63,26 @@ void gaussJordanElimination(double A[][9], double b[], int n) {
 			temp = A[j][i] / A[i][i];
 			for (int k = i; k < n + 1; k++) {
 				A[j][k] = round((A[j][k] - A[i][k] * temp) * 10000) / 10000;
+				//				A[j][k] = A[j][k] - A[i][k] * temp;
 			}
 		}
-		pivotVal = A[i][i];//must divide by double
+		pivotVal = A[i][i];// scaling all rows by the pivot value
 		for (int k = i; k < n + 1; k++) {
 			A[i][k] = A[i][k]/pivotVal;
 		}
 	}
-	cout << "Printing out GuassJordanElimination" << endl;
-	printMatrix(A, n);
-	cout << endl;
+
+	for(int i=1;i<n;i++){
+		for(int j=0;j<i;j++){
+			temp=A[j][i]/A[i][i];
+			for(int k=i;k<n+1;k++){
+				//A[j][k]=round((A[j][k] - A[i][k]*temp) *1000)/1000;
+				A[j][k]=A[j][k] - A[i][k]*temp;
+			}
+		}
+
+	}
+
 }
 
 /*
@@ -115,14 +97,4 @@ void printMatrix(double A[][9], int n) {
 	}
 }
 
-//ExtraCode from gaussJordanElimination
-/*
- * //  double pivotVal;
-//  scaling the rows by pivot values
-//	for (int i = 0; i < n; i++) {
-//		pivotVal = A[i][i];
-//		for (int j = i; j < n + 1; j++) {
-//			A[i][j] = A[i][j] / pivotVal;
-//		}
-//	}
- */
+
