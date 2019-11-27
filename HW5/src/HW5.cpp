da4e5b6ca4924bd195bdfc7c5a8761e993004a51
AlexG1031 <42430291+AlexG1031@users.noreply.github.com>
2019-11-27 17:00:24 -0500

    Part 1 of HW5 completed

---
+++ HW5/src/HW5.cpp
@@ -0,0 +1,74 @@
+//============================================================================
+// Name        : HW5.cpp
+// Author      : Alex Galvan && Suryansh Goyal
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include<iostream>
+#include<fstream>
+#include<string.h>
+
+using namespace std;
+
+void hashFile();
+string processString(string input);
+
+int C = 123;
+int m = 1000;
+
+int main() {
+//	readFileStackOverflow();
+	hashFile();
+	return 0;
+}
+
+/*
+ * Takes a word within file and determines
+ * the word's hash value
+ */
+void hashFile() {
+	ifstream file;
+	file.open("C:\\Users\\Alex Galvan\\Desktop\\CS-2223-Algorithms\\Raven.txt");
+	string word;
+	char x;
+	word.clear();
+
+	while (!file.eof()) {
+		x = file.get();
+
+		while (x != ' ') {
+			word = word + x;
+			x = file.get();
+		}
+
+		word = processString(word);
+		if (word != "") { //hash the words
+			//Levitin process
+			int h = 0;
+			int s = word.size();
+			for (int i = 0; i < s; i++) {
+				h = (h * C + word[i]) % m;
+			}
+			cout << word << ":  h value = " << h << endl;
+			word.clear();
+		}
+	}
+}
+
+/*
+ * Processes a string by accepting only a,..z,A,..Z,'
+ */
+string processString(string input) {
+	string result;
+	int len = input.length();
+	for (int i = 0; i < len; i++) {
+		if (isalpha(input[i]) != 0) {
+			result.push_back((input[i]));
+		} else if (input[i] == '\'') {
+			result.push_back((input[i]));
+		}
+	}
+	return result;
+}
