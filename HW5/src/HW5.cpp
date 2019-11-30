ef793ed83db0c298df7a1086644985f64646afcb
AlexG1031 <42430291+AlexG1031@users.noreply.github.com>
2019-11-30 02:32:01 -0500

    question 3, part a,b,c works; part d ugly but works; part e does not work

--- HW5/src/HW5.cpp
+++ HW5/src/HW5.cpp
@@ -14,7 +14,6 @@ using namespace std;
 
 void hashFile();
 string processString(string input);
-void func();
 
 int C = 123;
 int m = 1000;
@@ -24,8 +23,6 @@ int main() {
 	return 0;
 }
 
-
-
 /*
  * Takes a word within file and determines
  * the word's hash value
@@ -33,7 +30,97 @@ int main() {
 void hashFile() {
 	ifstream file;
 	file.open("Raven.txt");
-	string ravenmap[1000];
+	string ravenMap[1000];
+//	int ravenAddressCount[1000] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+//
+//	int ravenHashNumValue[1000] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
 	string word;
 	word.clear();
 	int h;
@@ -41,51 +128,219 @@ void hashFile() {
 
 	if (file.is_open()) {
 		while (!file.eof()) {
-
-
 			file >> word;
-			word=processString(word);
+			word = processString(word);
 			if (word != "") { //hash the words
 				//Levitin process
-
+				int hashNum;
 				h = 0;
 				s = word.size();
 				for (int i = 0; i < s; i++) {
 					h = (h * C + word[i]) % m;
+					hashNum = h;
 				}
-				cout << word << ":  h value = " << h<< " ";
+				cout << word << ":  h value = " << h << " ";
 
-				if(ravenmap[h]==word) { //word is in map
+				if (ravenMap[h] == word) { //word is in map
 					cout << " " << word << " is already in the hashmap" << endl;
 				}
 
-				else if (ravenmap[h]=="") {	//word isn't in hashmap
-					ravenmap[h]=word;
-					cout << " " << word << " has been added to the hashmap" << endl;
+				else if (ravenMap[h] == "") {	//word isn't in hashmap
+					ravenMap[h] = word;
+					cout << " " << word << " has been added to the hashmap"
+							<< endl;
+					//ravenAddressCount[h] = ravenAddressCount[h] + 1;
+					//ravenHashNumValue[h] = hashNum;
 				}
 
-				else if(ravenmap[h]!=word){ // already occupied
-					while(ravenmap[h]!=""){
-						if(ravenmap[h]==word){
-							cout << " " << word << " is already in the hashmap1" << endl;
+				else if (ravenMap[h] != word) { // already occupied
+					int count = 0;
+					while (ravenMap[h] != "") {
+						if (ravenMap[h] == word) {
+							cout << " " << word << " is already in the hashmap1"
+									<< endl;
 							break;
-						}
-					    else if(h==999){
-							h=0;
-						}
-						else{
+						} else if (h == 999) {
+							h = 0;
+							count++;
+						} else {
 							h++;
+							count++;
 						}
 					}
-					if(ravenmap[h]==""){
-						ravenmap[h]=word;
-						cout << " " << word << " has been added but at a different hash address" <<endl;
+					if (ravenMap[h] == "") {
+						ravenMap[h] = word;
+						cout << " " << word
+								<< " has been added but at a different hash address"
+								<< endl;
+						if ((h - count) < 0) {
+							//ravenAddressCount[h - count + 1000] =
+							//		ravenAddressCount[h - count + 1000] + 1;
+							//ravenHashNumValue[h - count + 1000] = hashNum;
+						} else {
+							//ravenAddressCount[h - count] = ravenAddressCount[h
+							//		- count] + 1;
+							//ravenHashNumValue[h - count] = hashNum;
+						}
 					}
 				}
 				word.clear();
 			}
 		}
 	}
+	cout << endl;
+	for (int i = 0; i < 1000; i++) {
+		cout << "index :" << i
+				<< " ravenmap[" << i << "]: " << ravenMap[i] <<endl;
+				//<< " Hash address count is " << ravenAddressCount[i]
+				//<< "  Hash number value is " << ravenHashNumValue[i] << endl;
+	}
+
+	// Question 3
+	// part a /////////////////////////////////////////////////
+	int nonEmptyAddress = 0;
+	for (int i = 0; i < 1000; i++) {
+		if (ravenMap[i] != "") {
+			nonEmptyAddress++;
+		}
+	}
+	cout << "part a" << endl;
+	cout << "non-empty addresses in table is " << nonEmptyAddress << endl;
+
+	// part b /////////////////////////////////////////////////
+	int stringSize = 1000;
+	int firstIndex = 0;
+	if (ravenMap[1000] != "") {
+		int i = 1000;
+		while (ravenMap[i] != "") {
+			i--;
+		}
+		firstIndex = i;
+	}
+	string ravenMapDuplicate[stringSize * 2];
+	for (int i = 0; i < stringSize; i++) {
+		ravenMapDuplicate[i] = ravenMapDuplicate[stringSize + i] = ravenMap[i];
+	}
+	int longestEmptyCount = 0;
+	int longestEmptyEndingIndex = 0;
+	int longestEmptyStartingIndex = 0;
+	int previousCount = 0;
+
+	for (int i = firstIndex; i < stringSize + firstIndex; i++) {
+		if (ravenMapDuplicate[i] == "") {
+			if (previousCount > longestEmptyCount) {
+				previousCount++;
+				longestEmptyCount = previousCount;
+				longestEmptyEndingIndex = i - firstIndex;
+			} else {
+				previousCount++;
+			}
+		} else {
+			previousCount = 0;
+		}
+	}
+
+	if ((longestEmptyEndingIndex - longestEmptyCount) < 0) { //count is bigger than longestEmptyCount
+		int temp = abs(longestEmptyEndingIndex - longestEmptyCount);
+		longestEmptyStartingIndex = 1000 - temp;
+	} else {
+		longestEmptyStartingIndex = longestEmptyEndingIndex - longestEmptyCount
+				+ 1;
+	}
+	cout << "part b" << endl;
+	cout << "The longest empty area in the table is " << longestEmptyCount
+			<< endl;
+	cout << "The starting index is " << longestEmptyStartingIndex
+			<< " and the ending index is " << longestEmptyEndingIndex << endl;
+
+	//part c   /////////////////////////////////////////////////
+	stringSize = 1000;
+	firstIndex = 0;
+	if (ravenMap[1000] == "") { //find what the first
+		int i = 1000;
+		while (ravenMap[i] == "") {
+			i--;
+		}
+		firstIndex = i;
+	}
+	string ravenMap2ndDuplicate[stringSize * 2];
+	for (int i = 0; i < stringSize; i++) {
+		ravenMap2ndDuplicate[i] = ravenMap2ndDuplicate[stringSize + i] =
+				ravenMap[i];
+	}
+
+	int longestNonEmptyCount = 0;
+	int longestNonEmptyEndingIndex = 0;
+	int longestNonEmptyStartingIndex = 0;
+	previousCount = 0;
+
+	for (int i = firstIndex; i < stringSize + firstIndex; i++) {
+		if (ravenMap2ndDuplicate[i] != "") {
+			if (previousCount > longestNonEmptyCount) {
+				previousCount++;
+				longestNonEmptyCount = previousCount + 1;
+				longestNonEmptyEndingIndex = i - firstIndex;
+			} else {
+				previousCount++;
+			}
+		} else {
+			previousCount = 0;
+		}
+	}
+	if ((longestNonEmptyEndingIndex - longestNonEmptyCount) < 0) { //count is bigger than longestEmptyCount
+		int temp = abs(longestNonEmptyEndingIndex - longestNonEmptyCount);
+		longestNonEmptyStartingIndex = 1000 - temp;
+	} else {
+		longestNonEmptyStartingIndex = longestNonEmptyEndingIndex
+				- longestNonEmptyCount + 1;
+	}
+	cout << "part c" << endl;
+	cout << "The longest non-empty area in the table is "
+			<< longestNonEmptyCount << endl;
+	cout << "The starting index is " << longestNonEmptyStartingIndex
+			<< " and the ending index is " << longestNonEmptyEndingIndex
+			<< endl;
+
+//	//part d   /////////////////////////////////////////////////
+//	int maxNum = 0;
+//	int currentNum = 0;
+//	int maxNumIndex = 0;
+//	for (int i = 0; i < 1000; i++) {
+//		currentNum = ravenAddressCount[i];
+//		if (currentNum > maxNum) {
+//			maxNum = currentNum;
+//			maxNumIndex = i;
+//		}
+//	}
+//	cout << "part d" << endl;
+//	//cout<<"max num is "<<maxNum <<endl;
+//	//cout<<"max num index is "<<maxNumIndex<<endl;
+//	cout << "the hash address with the greatest "
+//			"number of distinct words is " << ravenMap[maxNumIndex] << endl;
+//
+//	cout << "the number of words with that address is " << maxNum << endl;
+//	//part e   /////////////////////////////////////////////////
+//	int hashVal = 0;
+//	int difference = 0;
+//	int maxDiff = 0;
+//	string wrd;
+//
+//	for (int i = 0; i < 1000; i++) {
+//		if ((ravenAddressCount[i] == 0) || (ravenAddressCount[i] == 1)) {
+//		} else {
+//			hashVal = ravenHashNumValue[i];
+//			difference = ravenAddressCount[i] - hashVal;
+//			if (maxDiff < difference) {
+//				maxDiff = difference;
+//				wrd = ravenMap[i];
+//			}
+//		}
+//	}
+//	cout << "part e" << endl;
+//	cout << "word that is placed longest from its actual hash address is "
+//			<< wrd << endl;
+//	cout << "the distance away the word is from its actual hash address is "
+//			<< maxDiff << endl;
 }
 
 /*
