10fbccfdc1aeb94074200b77de2bf83dabf90a3a
Suryanshg <Suryanshg47@gmail.com>
2019-12-02 01:49:21 -0500

    Made the code for reading a little bit simpler

--- DijkstraAlgorithm/src/DijkstraAlgorithm.cpp
+++ DijkstraAlgorithm/src/DijkstraAlgorithm.cpp
@@ -11,35 +11,33 @@
 #include <sstream>
 using namespace std;
 
+
+
 int main() {
 	ifstream file;
-	string line;
 	file.open("DijkstraExample.txt");
 	int intMatrixSize;
-	if (file.good()) {
-		string stringMatrixSize;
-		getline(file, stringMatrixSize);
-		istringstream(stringMatrixSize) >> intMatrixSize; //intMatrixSize declared here
-		cout << "Matrix size is " << intMatrixSize << endl;
-		int fileMatrix[intMatrixSize][intMatrixSize];
-		for (int i = 0; i < intMatrixSize; i++) {
-			for (int j = 0; j < intMatrixSize; j++) {
-				int x;
-				file >> x;
-				fileMatrix[i][j] = x;
-			}
-		}
-		for (int i = 0; i < intMatrixSize; i++) {
-			for (int j = 0; j < intMatrixSize; j++) {
-				string space = ((fileMatrix[i][j] < 10) ? ("   ") : ("  "));
-				if(fileMatrix[i][j] > 99){
-					space = " ";
-				}
-				cout << fileMatrix[i][j]<< space;
-			}
-			cout << endl;
+	file>>intMatrixSize;
+	cout << "Matrix size is " << intMatrixSize << endl;
+	int fileMatrix[intMatrixSize][intMatrixSize];
+	for (int i = 0; i < intMatrixSize; i++) {
+		for (int j = 0; j < intMatrixSize; j++) {
+			int x;
+			file >> x;
+			fileMatrix[i][j] = x;
 		}
 	}
+	for (int i = 0; i < intMatrixSize; i++) {
+		for (int j = 0; j < intMatrixSize; j++) {
+			string space = ((fileMatrix[i][j] < 10) ? ("   ") : ("  "));
+			if(fileMatrix[i][j] > 99){
+				space = " ";
+			}
+			cout << fileMatrix[i][j]<< space;
+		}
+		cout << endl;
+	}
+
 	return 0;
 }
 /*
