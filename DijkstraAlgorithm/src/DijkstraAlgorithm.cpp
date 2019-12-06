0cb896320034c8c8c9ab133ceeacd65d2e6b391d
AlexG1031 <42430291+AlexG1031@users.noreply.github.com>
2019-12-06 02:17:08 -0500

    added Javadocs within Dijkstra

--- DijkstraAlgorithm/src/DijkstraAlgorithm.cpp
+++ DijkstraAlgorithm/src/DijkstraAlgorithm.cpp
@@ -14,16 +14,16 @@ using namespace std;
 
 string nodes[10] = { "A", "J", "M", "R", "K", "S", "I", "N", "T", "D" };
 
-int** determineMatrix(int size);
 int determineMatrixSize();
+int** determineMatrix(int size);
 void determineInfo(string *startNode, string *endNode, int size);
 int** performDijkstra(string startNode, string endNode, int **fileMatrix,
 		int size);
 bool contains(vector<int> vector, int node);
 int getIndex(string node);
-void printMatrix(int **matrix, int size);
 void produceResults(int **dijkstraGraph, string startNode, string endNode,
 		int size);
+void printMatrix(int **matrix, int size);
 
 int main() {
 	int size = determineMatrixSize();
@@ -34,30 +34,79 @@ int main() {
 	produceResults(dijkstraGraph, startNode, endNode, size);
 }
 
-void produceResults(int **dijkstraGraph, string startNode, string endNode,
-		int size) {
-	//Length of desired path
-	//Quickest route to endNode
-	int end = getIndex(endNode);
-	cout << "The shortest distance between " << startNode << " to " << endNode
-			<< " is " << dijkstraGraph[end][1] << endl;
-	cout << "Shortest Path from " << startNode << " to " << endNode << " is:"
-			<< endl;
-	vector<int> path;
-	path.push_back(end);
-	while (endNode != startNode) {
-		int index = getIndex(endNode);
-		path.push_back(dijkstraGraph[index][0]);
-		endNode = nodes[dijkstraGraph[index][0]];
-	}
-	size = path.size();
-	for (int i = size - 1; i >= 0; i--) {
-		string arrow = (i == 0) ? "" : " -> ";
-		cout << nodes[path[i]] << arrow;
-	}
-	cout << endl;
+/*
+ * Determines the size of the matrix and return it as an int
+ */
+int determineMatrixSize() {
+	ifstream file;
+	file.open("DijkstraExample.txt");
+	int intMatrixSize;
+	file >> intMatrixSize;
+	file.close();
+	return intMatrixSize;
 }
 
+/*
+ * a 2D matrix is produced based on the passed in file containing matrix
+ * returns the 2D matrix
+ */
+int** determineMatrix(int size) {
+	ifstream file;
+	file.open("DijkstraExample.txt");
+	int intMatrixSize;
+	file >> intMatrixSize;
+	int x;
+	int **fileMatrix = 0;
+	fileMatrix = new int*[size];
+	for (int i = 0; i < size; i++) {
+		fileMatrix[i] = new int[size];
+		for (int j = 0; j < size; j++) {
+			file >> x;
+			fileMatrix[i][j] = x;
+		}
+	}
+	file.close();
+	return fileMatrix;
+}
+
+/*
+ * Determines the start node and end node desired
+ * stores startNode and endNode
+ */
+void determineInfo(string *startNode, string *endNode, int size) {
+	cout << "Enter start node: " << endl;
+	bool startNodeValid = false;
+	while (!startNodeValid) {
+		cin >> *startNode;
+		for (int i = 0; i < size; i++) {
+			if (*startNode == nodes[i]) {
+				startNodeValid = true;
+			}
+		}
+		if (!startNodeValid) {
+			cout << "This node is not valid. Try again" << endl;
+		}
+	}
+	cout << "Enter end node: " << endl;
+	bool endNodeValid = false;
+	while (!endNodeValid) {
+		cin >> *endNode;
+		for (int i = 0; i < size; i++) {
+			if (*endNode == nodes[i]) {
+				endNodeValid = true;
+			}
+		}
+		if (!endNodeValid) {
+			cout << "This node is not valid. Try again" << endl;
+		}
+	}
+}
+
+/*
+ * Based on the inserted file, Dijkstra is performed, producing a graph of
+ * the distance, source, and visited
+ * Returns graph
+ */
 int** performDijkstra(string startNode, string endNode, int **fileMatrix,
 		int size) {
 	int start = getIndex(startNode);
@@ -131,6 +180,26 @@ int** performDijkstra(string startNode, string endNode, int **fileMatrix,
 	return Q;
 }
 
+/*
+ * Determines the corresponding node's index found in nodes[]
+ * Returns index
+ */
+int getIndex(string node) {
+	int index;
+	for (int i = 0; i < 10; i++) {
+		if (node == nodes[i]) {
+			index = i;
+			break;
+		}
+	}
+	return index;
+}
+
+/*
+ * Determines whether the node adjacent and determines whether it is in the vector
+ * return true if the node is in the vector
+ * return false if otherwise
+ */
 bool contains(vector<int> vector, int node) {
 	bool found = false;
 	int size = vector.size();
@@ -143,71 +212,32 @@ bool contains(vector<int> vector, int node) {
 	return found;
 }
 
-void determineInfo(string *startNode, string *endNode, int size) {
-	cout << "Enter start node: " << endl;
-	bool startNodeValid = false;
-	while (!startNodeValid) {
-		cin >> *startNode;
-		for (int i = 0; i < size; i++) {
-			if (*startNode == nodes[i]) {
-				startNodeValid = true;
-			}
-		}
-		if (!startNodeValid) {
-			cout << "This node is not valid. Try again" << endl;
-		}
+/*
+ * Produce the shortest distance from one node to another
+ * Also produces the node route from starting node to end node
+ */
+void produceResults(int **dijkstraGraph, string startNode, string endNode,
+		int size) {
+	//Length of desired path
+	//Quickest route to endNode
+	int end = getIndex(endNode);
+	cout << "The shortest distance between " << startNode << " to " << endNode
+			<< " is " << dijkstraGraph[end][1] << endl;
+	cout << "Shortest Path from " << startNode << " to " << endNode << " is:"
+			<< endl;
+	vector<int> path;
+	path.push_back(end);
+	while (endNode != startNode) {
+		int index = getIndex(endNode);
+		path.push_back(dijkstraGraph[index][0]);
+		endNode = nodes[dijkstraGraph[index][0]];
 	}
-	cout << "Enter end node: " << endl;
-	bool endNodeValid = false;
-	while (!endNodeValid) {
-		cin >> *endNode;
-		for (int i = 0; i < size; i++) {
-			if (*endNode == nodes[i]) {
-				endNodeValid = true;
-			}
-		}
-		if (!endNodeValid) {
-			cout << "This node is not valid. Try again" << endl;
-		}
+	size = path.size();
+	for (int i = size - 1; i >= 0; i--) {
+		string arrow = (i == 0) ? "" : " -> ";
+		cout << nodes[path[i]] << arrow;
 	}
-}
-
-int getIndex(string node) {
-	int index;
-	for (int i = 0; i < 10; i++) {
-		if (node == nodes[i]) {
-			index = i;
-			break;
-		}
-	}
-	return index;
-}
-int** determineMatrix(int size) {
-	ifstream file;
-	file.open("DijkstraExample.txt");
-	int intMatrixSize;
-	file >> intMatrixSize;
-	int x;
-	int **fileMatrix = 0;
-	fileMatrix = new int*[size];
-	for (int i = 0; i < size; i++) {
-		fileMatrix[i] = new int[size];
-		for (int j = 0; j < size; j++) {
-			file >> x;
-			fileMatrix[i][j] = x;
-		}
-	}
-	file.close();
-	return fileMatrix;
-}
-//Determines the size of the matrix
-int determineMatrixSize() {
-	ifstream file;
-	file.open("DijkstraExample.txt");
-	int intMatrixSize;
-	file >> intMatrixSize;
-	file.close();
-	return intMatrixSize;
+	cout << endl;
 }
 
 //Printing the Weighted Graph Matrix
