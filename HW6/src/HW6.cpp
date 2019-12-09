61f2710f323e19dca70934c5081cc407eafdb7e4
AlexG1031 <42430291+AlexG1031@users.noreply.github.com>
2019-12-09 02:17:41 -0500

    found first solution to n-queens problem. Takes too long to compile...

--- HW6/src/HW6.cpp
+++ HW6/src/HW6.cpp
@@ -14,18 +14,12 @@ bool isLegalPosition(vector<int> board, int n);
 vector<int> nextLegalPosition(vector<int> board, int n);
 bool hasDuplicate(vector<int> board);
 bool intersectDiagonally(vector<int> board);
-bool perfectSituation(vector<int> board, int n);
-bool noLegalPosition(vector<int> board, int n);
 vector<int> SUCCESSOR(vector<int> board, int n);
-vector<int> emptyBoard(vector<int> board, int n);
-vector<int> nextFullSolvedBoard(vector<int> board, int n);
-vector<int> fillBoard(vector<int> board, int n);
-vector<int> correctIllegalBoard(vector<int> board, int n);
-int getNumOfQueens(vector<int> board);
+bool emptyRow(vector<int> board, int n);
 void printBoard(vector<int> board);
 
 int main() {
-	vector<int> board = { 1, 6, 8, 3, 7, 4, 2, 5 };//{ 1, 6, 8, 3, 5, 0, 0, 0 }; // works well { 1, 6, 8, 3, 7, 0, 0, 0 }; // works well  //{ 1, 6, 8, 3, 7, 4, 2, 5 };
+	vector<int> board = { 1, 6, 8, 3, 7, 4, 2, 5 }; //{ 1, 6, 8, 3, 5, 0, 0, 0 }; // works well { 1, 6, 8, 3, 7, 0, 0, 0 }; // works well  //{ 1, 6, 8, 3, 7, 4, 2, 5 };
 	int n = board.size();
 	cout << "Initial board" << endl;
 	printBoard(board);
@@ -34,8 +28,24 @@ int main() {
 	board = nextLegalPosition(board, n);
 	cout << "IsLegalPosition result is " << legPos << endl;
 	cout << "Next Legal Position is" << endl;
-//	cout << "SUCCESSOR IS" << endl;
 	printBoard(board);
+//	cout << "SUCCESSOR IS" << endl;
+	for (int i = 4; i <= 100; i++) {
+		vector<int> newBoard;
+		for (int j = 0; j < i; j++) {
+			newBoard.push_back(1);
+		}
+		bool newBoardLegal = isLegalPosition(newBoard, i);
+		bool zeroRow = emptyRow(newBoard, i);
+		while (!(newBoardLegal && !zeroRow)) {
+			newBoard = nextLegalPosition(newBoard, i);
+			newBoardLegal = isLegalPosition(newBoard, i);
+			zeroRow = emptyRow(newBoard, i);
+		}
+		cout << "First board solution n = " << i << " is " << endl;
+		printBoard(newBoard);
+		newBoard.clear();
+	}
 }
 
 /*
@@ -61,23 +71,47 @@ bool isLegalPosition(vector<int> board, int n) {
  */
 vector<int> nextLegalPosition(vector<int> board, int n) {
 	vector<int> successor;
-	//works well
 	successor = SUCCESSOR(board, n);
-	while(!isLegalPosition(successor, n)){
+	while (!isLegalPosition(successor, n)) {
 		successor = SUCCESSOR(successor, n);
 	}
 	return successor;
 }
 
-int getNumOfQueens(vector<int> board) {
-	int boardSize = board.size();
-	int counter = 0;
-	for (int i = 0; i < boardSize; i++) {
-		if (board[i] != 0) {
-			counter++;
+/*
+ *
+ */
+bool hasDuplicate(vector<int> board) {
+	bool result = false;
+	int size = board.size();
+	for (int i = 0; i < size - 1; i++) {
+		for (int j = i + 1; j < size; j++) {
+			if ((board[i] == board[j]) && (board[i] != 0)) {
+				result = true;
+				break;
+			}
 		}
 	}
-	return counter;
+	return result;
+}
+
+/*
+ *
+ */
+bool intersectDiagonally(vector<int> board) {
+	bool intersect = false;
+	int size = board.size();
+	for (int i = 0; i < size; i++) {
+		for (int j = i + 1; j < size; j++) {
+			if (board[j] != 0) {
+				if (abs(board[i] - board[j]) == abs(i - j)) { //is a diagonal value
+					intersect = true;
+					break;
+				}
+			}
+		}
+	}
+	return intersect;
 }
 
 /*
@@ -137,94 +171,16 @@ vector<int> SUCCESSOR(vector<int> board, int n) {
 /*
  *
  */
-bool hasDuplicate(vector<int> board) {
-	bool result = false;
-	int size = board.size();
-	for (int i = 0; i < size - 1; i++) {
-		for (int j = i + 1; j < size; j++) {
-			if ((board[i] == board[j]) && (board[i] != 0)) {
-				result = true;
-				break;
-			}
-		}
-	}
-	return result;
-}
-
-/*
- *
- */
-bool intersectDiagonally(vector<int> board) {
-	bool intersect = false;
-	int size = board.size();
-	for (int i = 0; i < size; i++) {
-		for (int j = i + 1; j < size; j++) {
-			if (board[j] != 0) {
-				if (abs(board[i] - board[j]) == abs(i - j)) { //is a diagonal value
-					intersect = true;
-					break;
-				}
-			}
-		}
-	}
-	return intersect;
-}
-
-/*
- *
- */
-bool noLegalPosition(vector<int> board, int n) {
-	bool result = false;
-	return result;
-}
-
-/*
- *
- */
-bool perfectSituation(vector<int> board, int n) {
-	bool result = false;
-	return result;
-}
-
-/*
- *
- */
-bool fullySolved(vector<int> board, int n) {
-	bool result = false;
-	return result;
-}
-
-/*
- *
- */
-vector<int> emptyBoard(vector<int> board, int n) {
-	vector<int> emptyBoard;
+bool emptyRow(vector<int> board, int n) {
+	bool emptyRow = false;
 	int boardSize = board.size();
 	for (int i = 0; i < boardSize; i++) {
-		emptyBoard.insert(emptyBoard.begin(), 0);
+		if (board[i] == 0) {
+			emptyRow = true;
+			break;
+		}
 	}
-	return board;
-}
-
-/*
- *
- */
-vector<int> nextFullSolvedBoard(vector<int> board, int n) {
-	return board;
-}
-
-/*
- *
- */
-vector<int> fillBoard(vector<int> board, int n) {
-	return board;
-}
-
-/*
- *
- */
-vector<int> correctIllegalBoard(vector<int> board, int n) {
-	return board;
+	return emptyRow;
 }
 
 /*
